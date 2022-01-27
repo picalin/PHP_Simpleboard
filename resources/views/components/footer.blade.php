c9d381792610931708cf4543cf350c01e81c3816
picalin <s.hikaru.2125@gmail.com>
2022-01-27 22:09:42 +0900

    using layouts & header footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="py-3 bg-light">
+    <div class="container">
+        <span class="text-muted small">Simple Board by Laravel & Bootstrap 4</span>
+    </div>
+
+</footer>
--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -3,12 +3,18 @@
         <title>@yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- Bootstrap４を使用する -->
-        <link href="{{ asset('css/app.css' }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
+        @component('components.header')
+        @endcomponent
         <div class="container">
             @yield('content')
         </div>
+        
+        @component('components.footer')
+        @endcomponent
+
         <!-- Bootstrap4を使用する -->
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
