3c932384844aa181a540464a58d7545e1266eed1
picalin <s.hikaru.2125@gmail.com>
2022-02-05 17:59:24 +0900

    fix header link and set header sticky

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', 'PostController@index');
+
 Route::resource('posts', 'PostController');
 
 //HTTPS接続でアセット(CSSや画像など)を読み込むための処理
