045eb9d771b9d0d8fc7a6cfaf8cfcea2c6bd99b5
picalin <s.hikaru.2125@gmail.com>
2022-02-05 17:50:26 +0900

    fix footer and add viewpoint setting

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,8 @@
-<footer class="py-3 bg-light">
+<footer class="py-3 bg-light fixed-bottom">
     <div class="container">
         <span class="text-muted small">Simple Board by Laravel & Bootstrap 4</span>
     </div>
 
 </footer>
+
+
