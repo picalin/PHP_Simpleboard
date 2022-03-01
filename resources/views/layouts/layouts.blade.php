c9c0fa141820eb11d10bf11395c74d8011aaf79e
picalin <s.hikaru.2125@gmail.com>
2022-03-01 22:28:01 +0900

    fix flash message

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -3,13 +3,16 @@
         <title>@yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap４を使用する -->
+        {{-- Bootstrap４を使用する --}}
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         @component('components.header')
         @endcomponent
         <div class="container pt-5 pb-5">
+            {{-- componentを追加 --}}
+            @component('components.flash')
+            @endcomponent
             @yield('content')
         </div>
         
